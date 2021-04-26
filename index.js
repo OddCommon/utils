66911ced2cb00d605bd8dc9746809c9c3fe2bc78
Drew Dahlman <info@drewdahlman.com>
2021-04-26 07:54:39 -0400

    adding to index

--- index.js
+++ index.js
@@ -5,3 +5,4 @@ export { getQueryVar } from "./utils/browser.js";
 export { q, qAll } from "./utils/dom.js";
 export { randomNumber, formatTwoDigits } from "./utils/numbers.js";
 export { resolveEdgeImages } from "./utils/graphql.js";
+export { isMobile, isIPad } from "./utils/device.js";
